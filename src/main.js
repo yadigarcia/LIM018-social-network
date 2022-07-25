317a0a48046d4e0f7ec4202daf60355811b434ba
Yadira Garcia <leyadve@gmail.com>
2022-07-25 11:07:49 -0500

    actualizando main spa funcionando

--- src/main.js
+++ src/main.js
@@ -1,28 +1,17 @@
 // Este es el punto de entrada de tu aplicacion
-import { Home } from './component/Home.js';
-import { Register } from './component/Register.js';
+import { home } from './component/Home.js';
+import { register } from './component/Register.js';
 
 const rootDiv = document.getElementById('root');
 
-const Routes = {
-  './': Home,
-  './Register': Register,
+const routes = {
+  '/': home,
+  '/Register': register,
 };
 
-export const Navigation = (pathname) => {
+export const navigation = (pathname) => {
   window.history.pushState({}, pathname, window.location.origin + pathname);
-  rootDiv.appendChild(Routes[pathname]());
+  rootDiv.appendChild(routes[pathname]());
 };
 
-rootDiv.appendChild(Routes[window.location.pathname]());
-
-// const changeView = () => {
-//   switch (Route) {
-//     case './': return rootDiv.appendChild(Routes.Home());
-//     case './ Register': return rootDiv.appendChild(Routes.Register());
-//     default:
-//       break;
-//   }
-
-//   return changeView;
-// };
+rootDiv.appendChild(routes[window.location.pathname]());
