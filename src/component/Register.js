fa86ae1f384a3081b476337e804cb318af8a7890
Yadira Garcia <leyadve@gmail.com>
2022-07-26 01:16:22 -0500

    implementation CSS to view Home and Register

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,15 +1,32 @@
+// eslint-disable-next-line import/no-cycle
+import { navigation } from '../main.js';
+
 export const register = () => {
   const registerDiv = document.createElement('div');
+  registerDiv.classList.add('registerDiv');
 
-  registerDiv.textContent = 'Bienvenidos Viajeros';
+  const containerRegisterDiv = document.createElement('div');
+  containerRegisterDiv.classList.add('containerRegisterDiv');
+
+  const welcomeRegister = document.createElement('div');
+  welcomeRegister.classList.add('welcomeRegister');
+  welcomeRegister.textContent = 'Bienvenido Viajero!';
+
   const buttonRegister = document.createElement('button');
+  buttonRegister.classList.add('buttonStyle');
   const buttonBackToHome = document.createElement('button');
+  buttonBackToHome.classList.add('buttonStyle');
 
   buttonRegister.textContent = 'Registarse';
-  buttonBackToHome.textContent = 'Regresar';
+  buttonBackToHome.textContent = 'Ir al Inicio';
 
-  registerDiv.appendChild(buttonRegister);
-  registerDiv.appendChild(buttonBackToHome);
+  buttonRegister.addEventListener('click', () => navigation('/newUser'));
+  buttonBackToHome.addEventListener('click', () => navigation('/'));
+
+  registerDiv.appendChild(containerRegisterDiv);
+  registerDiv.appendChild(welcomeRegister);
+  containerRegisterDiv.appendChild(buttonRegister);
+  containerRegisterDiv.appendChild(buttonBackToHome);
 
   return registerDiv;
 };
--- src/main.js
+++ src/main.js
@@ -1,17 +1,28 @@
 // Este es el punto de entrada de tu aplicacion
 import { home } from './component/Home.js';
+import { login } from './component/login.js';
 import { register } from './component/Register.js';
 
 const rootDiv = document.getElementById('root');
 
 const routes = {
   '/': home,
-  '/Register': register,
+  '/register': register,
+  '/login': login,
 };
 
 export const navigation = (pathname) => {
   window.history.pushState({}, pathname, window.location.origin + pathname);
+
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.firstChild);
+  }
+
   rootDiv.appendChild(routes[pathname]());
 };
 
 rootDiv.appendChild(routes[window.location.pathname]());
+
+window.onpopstate = () => {
+  rootDiv.appendChild(routes[window.location.pathname]());
+};
