4b8695dff39069871a232eb23308b511be57e9b0
Yadira Garcia <leyadve@gmail.com>
2022-08-14 00:23:14 -0500

    Autentication concluded

--- src/component/login.js
+++ src/component/login.js
@@ -1,25 +1,45 @@
+/* eslint-disable no-shadow */
 /* eslint-disable no-alert */
 /* eslint-disable import/no-cycle */
 import { navigation } from '../main.js';
 import {
-  auth, signInWithEmailAndPassword, onAuthStateChanged, database, ref, update, signOut,
+  auth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  database,
+  ref,
+  update,
+  provider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+  providerf,
+  signOut,
 } from '../firebase/firebase.js';
 
 export const login = () => {
   const windowlogin = document.createElement('div');
   windowlogin.classList.add('windowlogin');
 
+  const logoLogin = document.createElement('div');
+  logoLogin.classList.add('logoLogin');
+  const imgLogo = document.createElement('img');
+  imgLogo.classList.add('imgLogo');
+  const nameLogo = document.createElement('p');
+  nameLogo.textContent = 'TRAVELLERS';
+  nameLogo.classList.add('nameLogo');
+
   const formLogin = document.createElement('form');
   formLogin.classList.add('formviewlogin');
 
-  const loginEmailDiv = document.createElement('div');
+  const loginImputDiv = document.createElement('div');
+  loginImputDiv.classList.add('loginImputDiv');
   const loginEmail = document.createElement('input');
-  loginEmail.classList.add('inputslogin');
+  loginEmail.classList.add('inputs');
   loginEmail.setAttribute('placeholder', 'Escribe tu Email');
 
-  const loginPaswordDiv = document.createElement('div');
   const loginPasword = document.createElement('input');
-  loginPasword.classList.add('inputslogin');
+  loginPasword.classList.add('inputs');
   loginPasword.setAttribute('type', 'password', 'required');
   loginPasword.setAttribute('placeholder', 'Escribe tu contraseña');
 
@@ -28,10 +48,19 @@ export const login = () => {
   buttonLogin.classList.add('buttonStyle');
   btnLoginDiv.classList.add('buttonStyleLogin');
 
+  const googleFacebook = document.createElement('div');
+  const btnGoogle = document.createElement('button');
+  const imgGoogle = document.createElement('img');
+  const btnFacebook = document.createElement('button');
+  const imgFacebook = document.createElement('img');
+  googleFacebook.classList.add('googleFacebook');
+  btnFacebook.classList.add('imgFacebook');
+  btnGoogle.classList.add('imgGoogle');
+
   const loginQuestionDiv = document.createElement('div');
   const loginQuestionCount = document.createElement('p');
   loginQuestionCount.classList.add('questionAboutCount');
-  loginQuestionDiv.classList.add('questionlogin');
+  loginQuestionDiv.classList.add('loginQuestionDiv');
   loginQuestionCount.textContent = '¿No tengo cuenta?';
 
   const lineLogin = document.createElement('p');
@@ -41,27 +70,32 @@ export const login = () => {
   btnToRegister.classList.add('buttonStyle');
 
   buttonLogin.textContent = 'Ingresar';
+  lineLogin.textContent = 'ingresar con';
   btnToRegister.textContent = 'Crear Cuenta';
-  lineLogin.textContent = 'Or';
-
-  // lineLogin.textContent = '_____________Or______________';
-
-  buttonLogin.addEventListener('click', () => navigation('/muro'));
-  btnToRegister.addEventListener('click', () => navigation('/register'));
 
   windowlogin.appendChild(formLogin);
 
-  loginEmailDiv.appendChild(loginEmail);
-  loginPaswordDiv.appendChild(loginPasword);
+  logoLogin.appendChild(imgLogo);
+  logoLogin.appendChild(nameLogo);
+  googleFacebook.appendChild(btnGoogle);
+  googleFacebook.appendChild(btnFacebook);
+  btnGoogle.appendChild(imgGoogle);
+  btnFacebook.appendChild(imgFacebook);
+
+  loginImputDiv.appendChild(loginEmail);
+  loginImputDiv.appendChild(loginPasword);
   loginQuestionDiv.appendChild(loginQuestionCount);
   btnLoginDiv.appendChild(buttonLogin);
+  btnLoginDiv.appendChild(lineLogin);
+  btnLoginDiv.appendChild(googleFacebook);
   btnLoginDiv.appendChild(loginQuestionDiv);
   btnLoginDiv.appendChild(btnToRegister);
 
-  formLogin.appendChild(loginEmailDiv);
-  formLogin.appendChild(loginPaswordDiv);
+  formLogin.appendChild(logoLogin);
+  formLogin.appendChild(loginImputDiv);
   formLogin.appendChild(btnLoginDiv);
-  formLogin.appendChild(lineLogin);
+
+  btnToRegister.addEventListener('click', () => navigation('/register'));
 
   buttonLogin.addEventListener('click', (e) => {
     e.preventDefault();
@@ -85,25 +119,80 @@ export const login = () => {
         const errorCode = error.code;
         const errorMessage = error.message;
 
-        alert(`${errorCode} ${errorMessage}`);
+        alert(`${errorMessage}`);
       });
+  });
 
-    // observador
-    /* const user = auth.currentUser;
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // ...
-        const uid = user.uid;
-        // ...
-      } else {
-        // User is signed out
-        // ...
-      }
-    }); */
+  // Ingreso con google-------------------------------------
+
+  btnGoogle.addEventListener('click', (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+
+        navigation('/muro');
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        alert(`${errorMessage}`);
+      });
+  });
+
+  // Ingreso con facebook-------------------------------------
+
+  btnFacebook.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    signInWithPopup(auth, providerf)
+      .then((result) => {
+      // The signed-in user info.
+        const user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+        navigation('/muro');
+      })
+      .catch((error) => {
+      // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        alert(`${errorMessage}`);
+      });
+  });
+
+  // observador------------------
+  const user = auth.currentUser;
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // ...
+      const uid = user.uid;
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
   });
 
   return windowlogin;
 };
+
 // para salir de la sesion
 // variableparasalir.addEventListener('click', (e) => {
 //   e.preventDefault();
@@ -111,7 +200,6 @@ export const login = () => {
 //   // Sign-out successful.
 //     alert('Estas seguro que quieres salir de la pagina');
 //   }).catch((error) => {
-//   // An error happened.
 //     const errorCode = error.code;
 //     const errorMessage = error.message;
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,12 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js';
 import {
-  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged, signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
 } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js';
 
 import {
@@ -31,10 +37,18 @@ export const app = initializeApp(firebaseConfig);
 // Autentication auth from Firebase
 
 export const database = getDatabase(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(app);
+export const providerf = new FacebookAuthProvider(app);
 
-export const auth = getAuth();
 export {
-  createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
 };
 export { set, ref, update };
 
